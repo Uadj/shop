2006858c03e570a8285c5e6c514746807fb01a8c
dgw <30551889+Uadj@users.noreply.github.com>
2022-01-07 18:39:29 +0900

    Initial commit

--- src/test/java/com/example/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/example/shop/repository/ItemRepositoryTest.java
@@ -7,13 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-@WebAppConfiguration
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 class ItemRepositoryTest {
@@ -41,6 +38,53 @@ class ItemRepositoryTest {
         List<Item> itemList =
                 itemRepository.findByItemNmOrItemDetail("테스트 상품", "테스트 상품 상세 설명5");
         for (Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 LessThan 테스트")
+    public void findByPriceLessThanTest(){
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByPriceLessThan(10005);
+        for (Item item :itemList){
+            System.out.println(item.toString());
+        }
+    }
+
+    @Test
+    @DisplayName("가격 내림차순 조회 테스트")
+    public void findByLessThanOrderByPriceDesc(){
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+        for (Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNm(){
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+    @Test
+    @DisplayName("@Quary를 이용한 상품 조회 테스트")
+    public void findByItemDetailTest(){
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByItemDetail("테스트 상품 상세 설명");
+        for(Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+    @Test
+    @DisplayName("nativeQuary를 이용한 상품 조회 테스트")
+    public void findByItemDetailByNative(){
+        this.createItemTest();
+        List<Item> itemList = itemRepository.findByItemDetailByNative("테스트 상품 상세 설명");
+        for(Item item : itemList){
             System.out.println(item.toString());
         }
     }
