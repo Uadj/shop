f4dff112e3782fdd7cd706ec940fba4109adbd38
dgw <30551889+Uadj@users.noreply.github.com>
2022-01-07 16:37:49 +0900

    Initial commit

---
+++ src/test/java/com/example/shop/repository/ItemRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.example.shop.repository;
+
+import com.example.shop.constant.ItemSellStatus;
+import com.example.shop.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@WebAppConfiguration
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest(){
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        Item savedItem = itemRepository.save(item);
+        System.out.println(savedItem.toString());
+    }
+    @Test
+    @DisplayName("상품명, 상품상세설명 or 테스트")
+    public void findByItemNmOrItemDetailTest(){
+        this.createItemTest();
+        List<Item> itemList =
+                itemRepository.findByItemNmOrItemDetail("테스트 상품", "테스트 상품 상세 설명5");
+        for (Item item : itemList){
+            System.out.println(item.toString());
+        }
+    }
+}
