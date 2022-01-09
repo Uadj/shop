74b8aba8de81ef6b0b1d40fa2c746a89769a2cb6
dgw <30551889+Uadj@users.noreply.github.com>
2022-01-09 12:56:12 +0900

    missing table member
    2022-01-09 12:55:28.336  WARN 16788 --- [  restartedMain] ConfigServletWebServerApplicationContext : Exception encountered during context initialization - cancelling refresh attempt: org.springframework.beans.factory.BeanCreationException: Error creating bean with name 'entityManagerFactory' defined in class path resource [org/springframework/boot/autoconfigure/orm/jpa/HibernateJpaConfiguration.class]: Invocation of init method failed; nested exception is javax.persistence.PersistenceException: [PersistenceUnit: default] Unable to build Hibernate SessionFactory; nested exception is org.hibernate.tool.schema.spi.SchemaManagementException: Schema-validation: missing table [member]

--- src/main/java/com/example/shop/controller/MainController.java
+++ src/main/java/com/example/shop/controller/MainController.java
@@ -1,13 +1,12 @@
 package com.example.shop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class MainController {
     @GetMapping(value = "/")
-    public String index(Model model){
-        return "index";
+    public String main(){
+        return "main";
     }
 }
---
+++ src/main/java/com/example/shop/service/MemberService.java
@@ -0,0 +1,29 @@
+package com.example.shop.service;
+
+import com.example.shop.entity.Member;
+import com.example.shop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Member saveMember(Member member){
+        validateDuplicateMember(member);
+        return memberRepository.save(member);
+    }
+
+    private void validateDuplicateMember(Member member){
+        Member findMember = memberRepository.findByEmail(member.getEmail());
+        if(findMember != null){
+            throw new IllegalStateException("이미 가입된 회원입니다.");
+        }
+    }
+}
